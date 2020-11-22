ed3754c4c3cad8220251ec704b7865198ca795a1
Andrei <andrei.pata@wtzconsult.com>
2020-11-22 13:06:59 +0200

    Changed from orderbyString to orderById

--- src/main/java/ro/patzzcode/appointmentPlatform/repositories/AppointmentRepository.java
+++ src/main/java/ro/patzzcode/appointmentPlatform/repositories/AppointmentRepository.java
@@ -14,10 +14,19 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long> 
 	Page<Appointment> findAll(Pageable pageable);
 
 	Page<Appointment> findByStatusOrderByAppointmentDateToStringDesc(Pageable pageable, String status);
+	
+	Page<Appointment> findByStatusOrderByIdDesc(Pageable pageable, String status);
 
 	Page<Appointment> findByClientOrderByAppointmentDateToStringDesc(Pageable pageable, User client);
+	
+	Page<Appointment> findByClientOrderByIdDesc(Pageable pageable, User client);
+
+	Page<Appointment> findByAppointmentDateToStringOrderByIdDesc(Pageable pageable, String appointmentDateToString);
 
 	Page<Appointment> findByClientAndStatusOrderByAppointmentDateToStringDesc(Pageable pageable, User client,
+			String status);
+	
+	Page<Appointment> findByClientAndStatusOrderByIdDesc(Pageable pageable, User client,
 			String status);
 
 	Page<Appointment> findByAppointmentDateToStringOrderByAppointmentDateToStringDesc(Pageable pageable,
@@ -28,11 +37,20 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long> 
 
 	Page<Appointment> findByAppointmentDateToStringAndStatusOrderByAppointmentDateToStringDesc(Pageable pageable,
 			String appointmentDateToString, String status);
+	
+	Page<Appointment> findByAppointmentDateToStringAndStatusOrderByIdDesc(Pageable pageable,
+			String appointmentDateToString, String status);
 
 	List<Appointment> findByAppointmentDateToStringAndValid(String appointmentDateToString, boolean valid);
 
 	List<Appointment> findByClientAndStatusOrderByAppointmentDateToStringDesc(User client, String status);
+	
+	List<Appointment> findByClientAndStatusOrderByIdDesc(User client, String status);
 
-	Page<Appointment> findByServiceAndValidOrderByAppointmentDateToStringDesc(Pageable pageable,String service, boolean valid);
+	Page<Appointment> findByServiceAndValidOrderByAppointmentDateToStringDesc(Pageable pageable, String service,
+			boolean valid);
+	
+	Page<Appointment> findByServiceAndValidOrderByIdDesc(Pageable pageable, String service,
+			boolean valid);
 
 }
